57f6208c17dd9ba1506cf87b156725b9bfcdc951
PHAN VAN HUYNH <june.utf@gmail.com>
2023-03-30 11:26:32 +0900

    app code
    start spring boot

---
+++ src/main/java/com/visa/kigen/KigenApplication.java
@@ -0,0 +1,13 @@
+package com.visa.kigen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KigenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KigenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/visa/kigen/KigenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.visa.kigen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KigenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
